fea228701d32c00dd8326cb7bb4c43b9002df5d7
Arthur Simões <amsbsimoes@gmail.com>
2025-12-05 13:19:55 -0300

    Initial commit: Project setup with Spring Boot, Cassandra, Redis dependencies and Architecture documentation

---
+++ src/test/java/com/Spectre/EncurtadorDeUrl/EncurtadorDeUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spectre.EncurtadorDeUrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
